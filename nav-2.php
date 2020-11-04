ae62dea3a53a540c9e6911f5c6129d25e3fac8c8
viliesov-d <73955647+viliesov-d@users.noreply.github.com>
2020-11-04 18:19:18 +0200

    lol

---
+++ nav-2.php
@@ -0,0 +1,6 @@
+<div id="footer">
+   <div>
+    &copy; Данило Вілєсов
+   </div>
+</div>
+
---
+++ nav.php
@@ -0,0 +1,6 @@
+<nav class="head-class"> 
+    <a href="index.php" class="button">Головна</a>
+    <a href="two-page.php" class="button">Half-life 2</a>
+    <a href="tre-page.php" class="button">PORTAL</a>
+    <a href="for-page.php" class="button">Resident</a>
+</nav>
